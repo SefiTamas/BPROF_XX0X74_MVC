e5c3270152577044eb6b5fde8e78b8008f790e27
Schäffer Tamás <SchafferTamas@stud.uni-obuda.hu>
2020-12-18 16:01:39 +0100

    WebApp/Views kész

---
+++ WebApp/wwwroot/Index.js
@@ -0,0 +1,249 @@
+﻿
+
+function submitForm() {
+    var submitionStr = "/Home/Sort?";
+    var a_products = document.getElementsByClassName("productTitle");
+    for (var i = 0; i < a_products.length; i++) {
+        if (i != 0) submitionStr += "&";
+        submitionStr += "products=" + a_products[i].id;
+    }
+    
+    var productlines_count = 0;
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type == "checkbox") {
+            if (inputs[i].name == "productLines") {
+                if (inputs[i].checked == true) {
+                    if (productlines_count != 0) submitionStr += "&";
+                    else submitionStr += "&&";
+                    submitionStr += "productLines=" + inputs[i].value;
+                    productlines_count++;
+                }
+            }
+        }
+    }
+    submitionStr += "&&";
+    var price_sorttype_count = 0;
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type == "checkbox") {
+            if (inputs[i].name == "price_sorttypeids") {
+                if (inputs[i].checked == true) {
+                    if (price_sorttype_count != 0) submitionStr += "&";
+                    else submitionStr += "&&";
+                    submitionStr += "price_sorttypeids=" + inputs[i].value;
+                    price_sorttype_count++;
+                }
+            }
+        }
+    }
+    submitionStr += "&&";
+    var age_sorttype_count = 0;
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type == "checkbox") {
+            if (inputs[i].name == "age_sorttypeids") {
+                if (inputs[i].checked == true) {
+                    if (age_sorttype_count != 0) submitionStr += "&";
+                    else submitionStr += "&&";
+                    submitionStr += "age_sorttypeids=" + inputs[i].value;
+                    age_sorttype_count++;
+                }
+            }
+        }
+    }
+    submitionStr += "&&";
+    var itemsc_sorttype_count = 0;
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type == "checkbox") {
+            if (inputs[i].name == "itemsc_sorttypeids") {
+                if (inputs[i].checked == true) {
+                    if (itemsc_sorttype_count != 0) submitionStr += "&";
+                    else submitionStr += "&&";
+                    submitionStr += "itemsc_sorttypeids=" + inputs[i].value;
+                    itemsc_sorttype_count++;
+                }
+            }
+        }
+    }
+    submitionStr += "&&";
+    var figuresc_sorttype_count = 0;
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type == "checkbox") {
+            if (inputs[i].name == "figuresc_sorttypeids") {
+                if (inputs[i].checked == true) {
+                    if (figuresc_sorttype_count != 0) submitionStr += "&";
+                    else submitionStr += "&&";
+                    submitionStr += "figuresc_sorttypeids=" + inputs[i].value;
+                    figuresc_sorttype_count++;
+                }
+            }
+        }
+    }
+    submitionStr += "&&";
+    var categories_count = 0;
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type == "checkbox") {
+            if (inputs[i].name == "categoryids") {
+                if (inputs[i].checked == true) {
+                    if (categories_count != 0) submitionStr += "&";
+                    else submitionStr += "&&";
+                    submitionStr += "categoryids=" + inputs[i].value;
+                    categories_count++;
+                }
+            }
+        }
+    }
+    
+    document.formsort.action = submitionStr;
+    document.getElementById("formsort").submit();
+}
+
+function SavePriceSort(newsort) {
+    var submitStr = "/Home/EditPriceSort?";    
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "pricemin") {
+            submitStr += "id=" + inputs[i].id;           
+        }       
+    }
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "pricemin") {          
+            submitStr += "&&pricemin=" + inputs[i].value.toString();
+        }
+        else if (inputs[i].name == "pricemax") {
+            submitStr += "&&pricemax=" + inputs[i].value.toString();           
+        }
+    }    
+    if (newsort != null) {
+        submitStr += "&&newsort=1";
+    }
+    var forms = document.getElementsByTagName("form");
+    for (var i = 0; i < forms.length; i++) {
+        if (forms[i].id == "masterform") {
+            forms[i].action = submitStr;
+            forms[i].submit();
+        }
+    }
+}
+
+
+function SaveItemsSort(newsort) {
+    var submitStr = "/Home/EditItemsSort?";
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "itemscmin") {
+            submitStr += "id=" + inputs[i].id;
+        }
+    }
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "itemscmin") {
+            submitStr += "&&itemscmin=" + inputs[i].value.toString();
+        }
+        else if (inputs[i].name == "itemscmax") {
+            submitStr += "&&itemscmax=" + inputs[i].value.toString();
+        }
+    }
+    if (newsort != null) {
+        submitStr += "&&newsort=1";
+    }
+    var forms = document.getElementsByTagName("form");
+    for (var i = 0; i < forms.length; i++) {
+        if (forms[i].id == "masterform") {
+            forms[i].action = submitStr;
+            forms[i].submit();
+        }
+    }
+}
+
+function SaveFiguresSort(newsort) {
+    var submitStr = "/Home/EditFiguresSort?";
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {        
+        if (inputs[i].name == "figuresc") {
+            submitStr += "id=" + inputs[i].id;
+        }
+    }
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "figurescmin") {
+            submitStr += "&&figurescmin=" + inputs[i].value.toString();
+        }
+        else if (inputs[i].name == "figurescmax") {
+            submitStr += "&&figurescmax=" + inputs[i].value.toString();
+        }
+        else if (inputs[i].name == "figuresc") {
+            submitStr += "&&figuresc=" + inputs[i].value.toString();
+        }
+    }
+    if (newsort != null) {
+        submitStr += "&&newsort=1";
+    }
+    var forms = document.getElementsByTagName("form");
+    for (var i = 0; i < forms.length; i++) {
+        if (forms[i].id == "masterform") {
+            forms[i].action = submitStr;
+            forms[i].submit();
+        }
+    }
+}
+
+
+function SaveAgeSort(newsort) {
+    var submitStr = "/Home/EditAgeSort?";
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {        
+        if (inputs[i].name == "age") {
+            submitStr += "id=" + inputs[i].id;
+        }
+    }
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "agemin") {
+            submitStr += "&&agemin=" + inputs[i].value.toString();
+        }
+        else if (inputs[i].name == "agemax") {
+            submitStr += "&&agemax=" + inputs[i].value.toString();
+        }
+        else if (inputs[i].name == "age") {
+            submitStr += "&&age=" + inputs[i].value.toString();
+        }
+    }
+    if (newsort != null) {
+        submitStr += "&&newsort=1";
+    }
+    var forms = document.getElementsByTagName("form");
+    for (var i = 0; i < forms.length; i++) {
+        if (forms[i].id == "masterform") {
+            forms[i].action = submitStr;
+            forms[i].submit();
+        }
+    }    
+}
+
+function SaveCategorySort(newsort) {
+    var submitStr = "/Home/EditCategorySort?";
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "categ") {
+            submitStr += "id=" + inputs[i].id;
+        }
+    }
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "categ") {
+            submitStr += "&&categ=" + inputs[i].value.toString();
+        }        
+    }
+    if (newsort != null) {
+        submitStr += "&&newsort=1";
+    }
+    var forms = document.getElementsByTagName("form");
+    for (var i = 0; i < forms.length; i++) {
+        if (forms[i].id == "masterform") {
+            forms[i].action = submitStr;
+            forms[i].submit();
+        }
+    }
+}
+
---
+++ WebApp/wwwroot/Product.js
@@ -0,0 +1,290 @@
+﻿
+window.onload = function () {
+    checkedopacity(null);
+}
+
+function checkedopacity(li) {  
+    var selectedli = "";
+    if (li != null)
+        selectedli = li;
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].className == "radio") {
+            if (inputs[i].id == "age") {
+                if ((inputs[i].checked == true && selectedli == "age") || (inputs[i].checked == true && selectedli == "")) {  
+                    found = "age";
+                    var lis = document.getElementsByTagName("li");
+                    for (var k = 0; k < lis.length; k++) {
+                        if (lis[k].id == "li-agemin") {
+                            lis[k].style.opacity = 0.5;
+                        }
+                    }                    
+                    lis = document.getElementsByTagName("li");
+                    for (var k = 0; k < lis.length; k++) {
+                        if (lis[k].id == "li-agemax") {
+                            lis[k].style.opacity = 0.5;
+                        }
+                    }
+                }
+                else{
+                    inputs[i].checked = false;
+                    var lis = document.getElementsByTagName("li");
+                    for (var k = 0; k < lis.length; k++) {
+                        if (lis[k].id == "li-agemin") {
+                            lis[k].style.opacity = 1;
+                        }
+                    }
+                    lis = document.getElementsByTagName("li");
+                    for (var k = 0; k < lis.length; k++) {
+                        if (lis[k].id == "li-agemax") {
+                            lis[k].style.opacity = 1;
+                        }
+                    }
+                }
+            }
+            else if (inputs[i].id == "agerange") {
+                if ((inputs[i].checked == true && selectedli == "agerange") || (inputs[i].checked == true && selectedli == "")) {
+                    found = "agerange";
+                    var lis = document.getElementsByTagName("li");
+                    for (var k = 0; k < lis.length; k++) {
+                        if (lis[k].id == "li-age") {
+                            lis[k].style.opacity = 0.5;
+                        }
+                    }
+                }
+                else {
+                    inputs[i].checked = false;
+                    var lis = document.getElementsByTagName("li");
+                    for (var k = 0; k < lis.length; k++) {
+                        if (lis[k].id == "li-age") {
+                            lis[k].style.opacity = 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+function CollectInfo() {
+    var submitStr = "";
+
+    var p = document.getElementsByTagName("p");
+    var productid;
+    for (var i = 0; i < p.length; i++) {
+        if (p[i].className == "p-title")
+            productid = p[i].id;
+    }
+    submitStr += "productid=" + productid;
+
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "productline") {
+            submitStr += "&&productline=" + inputs[i].value;
+        }
+    }
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].className == "radio") {
+            if (inputs[i].checked == true) {
+                if (inputs[i].id == "age") {
+                    for (var j = 0; j < inputs.length; j++) {
+                        if (inputs[j].name == "recommendedage") {
+                            submitStr += "&&age=" + inputs[j].value.toString();
+                        }
+                    }
+                }
+                else if (inputs[i].id == "agerange") {
+                    for (var j = 0; j < inputs.length; j++) {
+                        if (inputs[j].name == "recommendedagemin") {
+                            submitStr += "&&agemin=" + inputs[j].value.toString();
+                        }
+                        else if (inputs[j].name == "recommendedagemax") {
+                            submitStr += "&&agemax=" + inputs[j].value.toString();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "itemscount") {
+            submitStr += "&&itemsc=" + inputs[i].value.toString();
+        }
+        else if (inputs[i].name == "figurescount") {
+            submitStr += "&&figuresc=" + inputs[i].value.toString();
+        }
+    }
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "title") {
+            submitStr += "&&title=" + inputs[i].value;
+        }
+    }
+
+    var textareas = document.getElementsByTagName("textarea");
+    for (var i = 0; i < textareas.length; i++) {
+        if (textareas[i].name == "description") {
+            submitStr += "&&description=" + textareas[i].value;
+        }
+    }
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "price") {
+            submitStr += "&&price=" + inputs[i].value.toString();
+        }
+    }
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "thumbnail") {
+            submitStr += "&&thumbnail=" + inputs[i].value;
+        }
+    }
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name == "bigpicture") {
+            submitStr += "&&bigpicture=" + inputs[i].value;
+        }
+    }
+
+    return submitStr;
+}
+
+
+
+
+function addcategory() {
+    var submitStr = "/Home/AddCategoryToProduct?" + CollectInfo();
+  
+    var count = 0;
+    var IsEmptyOption = false;
+    var selects = document.getElementsByTagName("select");
+    for (var i = 0; i < selects.length; i++) {
+        if (selects[i].name = "categories") {
+            for (var j = 0; j < selects[i].options.length; j++) {
+                if (selects[i].options[j].selected == true) {
+                    if (selects[i].options[j].id != "null") {
+                        if (count == 0) submitStr += "&&";
+                        else submitStr += "&";
+                        submitStr += "categories=" + selects[i].options[j].id;
+                        count++;
+                    }
+                    else IsEmptyOption = true;
+                }
+            }
+        }
+    }
+
+    //var inputs = document.getElementsByTagName("input");
+    //for (var i = 0; i < inputs.length; i++) {
+    //    if (inputs[i].name == "productline") {
+    //        inputs[i].value = CollectInfo();
+    //    }
+    //}   
+
+   
+
+    if (!IsEmptyOption) {        
+        document.getElementById("addcateg").action = submitStr;
+        document.getElementById("addcateg").submit();
+    }
+
+    //document.addcateg.action = submitStr;
+}
+
+
+function removecategory(form) {
+    var submitStr = "/Home/RemoveCategoryFromProduct?" + CollectInfo();
+
+    
+    var IsEmptyOption = false;
+    var count = 0;
+    var liid = "li" + form.id.substring(4, form.id.length);
+    var lis = document.getElementsByClassName("c-name");
+    for (var i = 0; i < lis.length; i++) {
+        if (lis[i].id != liid) {
+            var lielem = lis[i].getElementsByTagName("select");
+            for (var j = 0; j < lielem.length; j++) {
+                if (lielem[j].name == "categories") {
+                    for (var k = 0; k < lielem[j].options.length; k++) {
+                        if (lielem[j].options[k].id != "null") {
+                            if (lielem[j].options[k].selected == true) {                                
+                                if (count == 0) submitStr += "&&";
+                                else submitStr += "&";
+                                submitStr += "categories=" + lielem[j].options[k].id;
+                                count++;                                                                 
+                            }
+                        }
+                        else IsEmptyOption = true;
+                    }
+                }
+            }
+        }
+    } 
+
+    if (form.id == "addcateg")
+        IsEmptyOption = false;
+   
+    if (IsEmptyOption)
+        submitStr += "&&nullcateg=true";
+  
+    form.action = submitStr;
+    form.submit();
+}
+
+
+function removeemptycategory() {
+    var submitStr = "/Home/RemoveCategoryFromProduct?" + CollectInfo();
+    
+    var count = 0;
+    var lis = document.getElementsByClassName("c-name");
+    for (var i = 0; i < lis.length; i++) {
+        if (lis[i].id.substring(0, 2) == "li") {
+            var selects = lis[i].getElementsByTagName("select");
+            for (var j = 0; j < selects.length; j++) {
+                if (selects[j].name == "categories") {
+                    for (var k = 0; k < selects[j].options.length; k++) {
+                        if (selects[j].options[k].selected == true) {
+                            if (count == 0) submitStr += "&&";
+                            else submitStr += "&";
+                            submitStr += "categories=" + selects[j].options[k].id;
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    var form = document.getElementById("addcateg");
+    form.action = submitStr;
+    form.submit();
+}
+
+function savechanges(newpro) {
+    var submitStr = "/Home/SaveChanges?" + CollectInfo();
+    
+    var count = 0;    
+    var selects = document.getElementsByTagName("select");
+    for (var i = 0; i < selects.length; i++) {
+        if (selects[i].name = "categories") {
+            for (var j = 0; j < selects[i].options.length; j++) {
+                if (selects[i].options[j].selected == true) {
+                    if (selects[i].options[j].id != "null") {
+                        if (count == 0) submitStr += "&&";
+                        else submitStr += "&";
+                        submitStr += "categories=" + selects[i].options[j].id;
+                        count++;
+                    }                    
+                }
+            }
+        }
+    }  
+    if (newpro != null) {
+        submitStr += "&&newpro=1";
+    }
+    var form = document.getElementById("addcateg");
+    form.action = submitStr;
+    form.submit();
+}
