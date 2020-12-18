2c17b893bc6381233a89b7b624a32ebb3d3006c7
Schäffer Tamás <SchafferTamas@stud.uni-obuda.hu>
2020-12-18 15:46:30 +0100

    Test/Non-CRUD tesztek kész

--- Test/LogicTest.cs
+++ Test/LogicTest.cs
@@ -120,5 +120,187 @@ namespace Test
             itemscRepo.Verify(repo => repo.Delete(It.IsAny<string>()), Times.Never);
             itemscRepo.Verify(repo => repo.Update(It.IsAny<string>(), It.IsAny<ItemsCount_SortType>()), Times.Once);
         }
+
+        [Test] // Non-Crud: GetProductCategory()
+        public void TestGetProductCategory()
+        {
+            Mock<IRepository<Category>> categoryRepo = new Mock<IRepository<Category>>(MockBehavior.Loose);
+            Mock<IRepository<Product>> productRepo = new Mock<IRepository<Product>>(MockBehavior.Loose);
+            Mock<IRepository<ProductCategory>> productCategRepo = new Mock<IRepository<ProductCategory>>(MockBehavior.Loose);
+            List<ProductCategory> productCategs = new List<ProductCategory>()
+            {
+                new ProductCategory() { CategoryId = "asdfghjkl123", ProductId = "qwertzuiop456" },
+                new ProductCategory() { CategoryId = "923843298gh", ProductId = "tzpojpmtj" },
+                new ProductCategory() { CategoryId = "trthorthothof", ProductId = "hjk,áhkjh" },
+                new ProductCategory() { CategoryId = "fejwnfjkwn", ProductId = "qwertzuiop456" },
+                new ProductCategory() { CategoryId = "asdassdasad", ProductId = "qwertzuiop456" },
+                new ProductCategory() { CategoryId = "j24jk23jk4j6b", ProductId = "mmhmghjfphdgs" }
+            };
+            List<Category> categories = new List<Category>()
+            {
+                new Category() { Id = "asdfghjkl123" },
+                new Category() { Id = "923843298gh" },
+                new Category() { Id = "trthorthothof" },
+                new Category() { Id = "fejwnfjkwn" },
+                new Category() { Id = "asdassdasad" },
+                new Category() { Id = "j24jk23jk4j6b" },
+                new Category() { Id = "dwmfnaifgpabigb" },
+                new Category() { Id = "923hh239h3ths" }
+            };
+            List<Category> expectedCategories = new List<Category>()
+            {
+                new Category() { Id = "asdfghjkl123" },
+                new Category() { Id = "fejwnfjkwn" },
+                 new Category() { Id = "asdassdasad" }
+            };
+            productCategRepo.Setup(repo => repo.Read()).Returns(productCategs.AsQueryable());
+            categoryRepo.Setup(repo => repo.Read()).Returns(categories.AsQueryable());
+            CategoryLogic categoryLogic = new CategoryLogic(categoryRepo.Object, productRepo.Object, productCategRepo.Object);
+
+            var result = categoryLogic.GetProductCategories("qwertzuiop456");
+
+            Assert.That(result.Count, Is.EqualTo(expectedCategories.Count));
+            Assert.That(result, Is.EquivalentTo(expectedCategories));
+
+            productCategRepo.Verify(repo => repo.Read(), Times.Once);
+            productCategRepo.Verify(repo => repo.Read(It.IsAny<string>()), Times.Never);
+            productCategRepo.Verify(repo => repo.Add(It.IsAny<ProductCategory>()), Times.Never);
+            productCategRepo.Verify(repo => repo.Delete(It.IsAny<string>()), Times.Never);
+            productCategRepo.Verify(repo => repo.Update(It.IsAny<string>(), It.IsAny<ProductCategory>()), Times.Never);
+
+            categoryRepo.Verify(repo => repo.Read(), Times.Once);
+            categoryRepo.Verify(repo => repo.Delete(It.IsAny<string>()), Times.Never);
+            categoryRepo.Verify(repo => repo.Read(It.IsAny<string>()), Times.Never);
+            categoryRepo.Verify(repo => repo.Add(It.IsAny<Category>()), Times.Never);
+            categoryRepo.Verify(repo => repo.Update(It.IsAny<string>(), It.IsAny<Category>()), Times.Never);
+
+            productRepo.Verify(repo => repo.Read(), Times.Never);
+            productRepo.Verify(repo => repo.Read(It.IsAny<string>()), Times.Never);
+            productRepo.Verify(repo => repo.Add(It.IsAny<Product>()), Times.Never);
+            productRepo.Verify(repo => repo.Delete(It.IsAny<string>()), Times.Never);
+            productRepo.Verify(repo => repo.Update(It.IsAny<string>(), It.IsAny<Product>()), Times.Never);
+        }
+
+        [Test] // Non-Crud: SortByProductLine()
+        public void TestSortByProductLine()
+        {
+            Mock<IRepository<Category>> categoryRepo = new Mock<IRepository<Category>>(MockBehavior.Loose);
+            Mock<IRepository<Product>> productRepo = new Mock<IRepository<Product>>(MockBehavior.Loose);
+            Mock<IRepository<ProductCategory>> productCategRepo = new Mock<IRepository<ProductCategory>>(MockBehavior.Loose);
+            Mock<IRepository<Price_SortType>> price_SortRepo = new Mock<IRepository<Price_SortType>>();
+            Mock<IRepository<ItemsCount_SortType>> itemsc_SortRepo = new Mock<IRepository<ItemsCount_SortType>>();
+            Mock<IRepository<FiguresCount_SortType>> figuresc_SortRepo = new Mock<IRepository<FiguresCount_SortType>>();
+            Mock<IRepository<Age_SortType>> age_SortRepo = new Mock<IRepository<Age_SortType>>();
+            ProductLogic productLogic = new ProductLogic(productRepo.Object, price_SortRepo.Object, itemsc_SortRepo.Object, figuresc_SortRepo.Object, categoryRepo.Object, age_SortRepo.Object, productCategRepo.Object);
+            List<Product> products = new List<Product>()
+            {
+                new Product(){ ProductLine = "qweqwe" },
+                new Product(){ ProductLine = "rtzrtz" },
+                new Product(){ ProductLine = "asdasd" },
+                new Product(){ ProductLine = "asdasd" },
+                new Product(){ ProductLine = "qweqwe" },
+                new Product(){ ProductLine = "rtzrtz" },
+                new Product(){ ProductLine = "iopiop" },
+                new Product(){ ProductLine = "jkljkl" },
+                new Product(){ ProductLine = "bnmbnm" },
+                new Product(){ ProductLine = "cvbcvb" }
+            };
+            List<string> productLines = new List<string>()
+            {
+                "asdasd", "bnmbnm", "rtzrtz"
+            };
+            List<Product> expectedproducts = new List<Product>()
+            {
+                new Product(){ ProductLine = "rtzrtz" },
+                new Product(){ ProductLine = "asdasd" },
+                new Product(){ ProductLine = "asdasd" },
+                new Product(){ ProductLine = "rtzrtz" },
+                new Product(){ ProductLine = "bnmbnm" }
+            };
+
+            var result = productLogic.SortByProductLine(products, productLines);
+
+            Assert.That(result.Count, Is.EqualTo(expectedproducts.Count));
+            Assert.That(result, Is.EquivalentTo(expectedproducts));
+
+            productRepo.Verify(repo => repo.Read(), Times.Never);
+            productRepo.Verify(repo => repo.Read(It.IsAny<string>()), Times.Never);
+            productRepo.Verify(repo => repo.Add(It.IsAny<Product>()), Times.Never);
+            productRepo.Verify(repo => repo.Delete(It.IsAny<string>()), Times.Never);
+            productRepo.Verify(repo => repo.Update(It.IsAny<string>(), It.IsAny<Product>()), Times.Never);
+
+            categoryRepo.Verify(repo => repo.Read(), Times.Never);
+            categoryRepo.Verify(repo => repo.Read(It.IsAny<string>()), Times.Never);
+            categoryRepo.Verify(repo => repo.Add(It.IsAny<Category>()), Times.Never);
+            categoryRepo.Verify(repo => repo.Delete(It.IsAny<string>()), Times.Never);
+            categoryRepo.Verify(repo => repo.Update(It.IsAny<string>(), It.IsAny<Category>()), Times.Never);
+
+            productCategRepo.Verify(repo => repo.Read(), Times.Never);
+            productCategRepo.Verify(repo => repo.Read(It.IsAny<string>()), Times.Never);
+            productCategRepo.Verify(repo => repo.Add(It.IsAny<ProductCategory>()), Times.Never);
+            productCategRepo.Verify(repo => repo.Delete(It.IsAny<string>()), Times.Never);
+            productCategRepo.Verify(repo => repo.Update(It.IsAny<string>(), It.IsAny<ProductCategory>()), Times.Never);
+        }
+
+        [Test] // Non-Crud: SortByItemsCount()
+        public void TestSortByItemsCount()
+        {
+            Mock<IRepository<Category>> categoryRepo = new Mock<IRepository<Category>>(MockBehavior.Loose);
+            Mock<IRepository<Product>> productRepo = new Mock<IRepository<Product>>(MockBehavior.Loose);
+            Mock<IRepository<ProductCategory>> productCategRepo = new Mock<IRepository<ProductCategory>>(MockBehavior.Loose);
+            Mock<IRepository<Price_SortType>> price_SortRepo = new Mock<IRepository<Price_SortType>>();
+            Mock<IRepository<ItemsCount_SortType>> itemsc_SortRepo = new Mock<IRepository<ItemsCount_SortType>>();
+            Mock<IRepository<FiguresCount_SortType>> figuresc_SortRepo = new Mock<IRepository<FiguresCount_SortType>>();
+            Mock<IRepository<Age_SortType>> age_SortRepo = new Mock<IRepository<Age_SortType>>();
+            ProductLogic productLogic = new ProductLogic(productRepo.Object, price_SortRepo.Object, itemsc_SortRepo.Object, figuresc_SortRepo.Object, categoryRepo.Object, age_SortRepo.Object, productCategRepo.Object);
+            List<Product> products = new List<Product>()
+            {
+                new Product(){ ItemsCount_SortTypeId = "qweqwe" },
+                new Product(){ ItemsCount_SortTypeId = "rtzrtz" },
+                new Product(){ ItemsCount_SortTypeId = "asdasd" },
+                new Product(){ ItemsCount_SortTypeId = "asdasd" },
+                new Product(){ ItemsCount_SortTypeId = "qweqwe" },
+                new Product(){ ItemsCount_SortTypeId = "rtzrtz" },
+                new Product(){ ItemsCount_SortTypeId = "iopiop" },
+                new Product(){ ItemsCount_SortTypeId = "jkljkl" },
+                new Product(){ ItemsCount_SortTypeId = "bnmbnm" },
+                new Product(){ ItemsCount_SortTypeId = "cvbcvb" }
+            };
+            List<string> itemscount_sorttypeIds = new List<string>()
+            {
+                "asdasd", "bnmbnm", "rtzrtz"
+            };
+            List<Product> expectedproducts = new List<Product>()
+            {
+                new Product(){ ItemsCount_SortTypeId = "rtzrtz" },
+                new Product(){ ItemsCount_SortTypeId = "asdasd" },
+                new Product(){ ItemsCount_SortTypeId = "asdasd" },
+                new Product(){ ItemsCount_SortTypeId = "rtzrtz" },
+                new Product(){ ItemsCount_SortTypeId = "bnmbnm" }
+            };
+
+            var result = productLogic.SortByItemsCount(products, itemscount_sorttypeIds);
+
+            Assert.That(result.Count, Is.EqualTo(expectedproducts.Count));
+            Assert.That(result, Is.EquivalentTo(expectedproducts));
+
+            productRepo.Verify(repo => repo.Read(), Times.Never);
+            productRepo.Verify(repo => repo.Read(It.IsAny<string>()), Times.Never);
+            productRepo.Verify(repo => repo.Add(It.IsAny<Product>()), Times.Never);
+            productRepo.Verify(repo => repo.Delete(It.IsAny<string>()), Times.Never);
+            productRepo.Verify(repo => repo.Update(It.IsAny<string>(), It.IsAny<Product>()), Times.Never);
+
+            categoryRepo.Verify(repo => repo.Read(), Times.Never);
+            categoryRepo.Verify(repo => repo.Read(It.IsAny<string>()), Times.Never);
+            categoryRepo.Verify(repo => repo.Add(It.IsAny<Category>()), Times.Never);
+            categoryRepo.Verify(repo => repo.Delete(It.IsAny<string>()), Times.Never);
+            categoryRepo.Verify(repo => repo.Update(It.IsAny<string>(), It.IsAny<Category>()), Times.Never);
+
+            productCategRepo.Verify(repo => repo.Read(), Times.Never);
+            productCategRepo.Verify(repo => repo.Read(It.IsAny<string>()), Times.Never);
+            productCategRepo.Verify(repo => repo.Add(It.IsAny<ProductCategory>()), Times.Never);
+            productCategRepo.Verify(repo => repo.Delete(It.IsAny<string>()), Times.Never);
+            productCategRepo.Verify(repo => repo.Update(It.IsAny<string>(), It.IsAny<ProductCategory>()), Times.Never);
+        }
     }
 }
