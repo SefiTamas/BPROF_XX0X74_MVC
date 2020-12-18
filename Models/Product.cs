15627e630297d50b124bb684c8ad609a79257687
Schäffer Tamás <SchafferTamas@stud.uni-obuda.hu>
2020-12-18 13:56:33 +0100

    Models/Product class kész

---
+++ Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Product
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [StringLength(200)]
+        public string ProductLine { get; set; }
+
+        [Range(1, 1000000)]
+        public int ItemsCount { get; set; }
+
+        [Range(0, 1000000)]
+        public int FiguresCount { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Range(0, 10000000)]
+        public int Price { get; set; }
+
+        [StringLength(200)]
+        public string PictureName_thumbnail { get; set; }
+
+        [StringLength(200)]
+        public string PictureName_big { get; set; }
+    }
+}
---
+++ sztf-szakirany_felevesproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sztf_szakirany_felevesproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
