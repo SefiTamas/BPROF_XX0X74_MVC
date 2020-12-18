6b3348d871c22ff27a30812f05c7adc9e4aa8e17
Schäffer Tamás <SchafferTamas@stud.uni-obuda.hu>
2020-12-18 14:42:12 +0100

    Repository/CategoryRepository kész

---
+++ Repository/CategoryRepository.cs
@@ -0,0 +1,57 @@
+﻿using Data;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class CategoryRepository : IRepository<Category>
+    {
+		DataContext context { get; set; }
+		public CategoryRepository(DataContext context)
+		{
+			this.context = context;
+		}
+
+		public void Add(Category item)
+		{
+			context.Categories.Add(item);
+			context.SaveChanges();
+		}
+
+		public void Delete(Category item)
+		{
+			context.Categories.Remove(item);
+			context.SaveChanges();
+		}
+
+		public void Delete(string Id)
+		{
+			Delete(Read(Id));
+		}
+
+		public Category Read(string Id)
+		{
+			return context.Categories.FirstOrDefault(t => t.Id == Id);
+		}
+
+		public IQueryable<Category> Read()
+		{
+			return context.Categories.AsQueryable();
+		}
+
+		public void Update(string oldid, Category newitem)
+		{
+			var olditem = Read(oldid);
+			olditem.Name = newitem.Name;
+			context.SaveChanges();
+		}
+
+		public void Save()
+		{
+			context.SaveChanges();
+		}
+	}
+}
