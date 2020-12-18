9dcee4d0c735ff6044da5ab6461214b91340d406
Schäffer Tamás <SchafferTamas@stud.uni-obuda.hu>
2020-12-18 14:36:55 +0100

    Repository/IRepository interface kész

---
+++ Repository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Repository
+{
+	public interface IRepository<T> where T : new()
+	{
+		void Add(T item);
+
+		void Delete(T item);
+
+		void Delete(string Id);
+
+		T Read(string Id);
+
+		IQueryable<T> Read();
+
+		void Update(string oldid, T newitem);
+
+		void Save();
+	}
+}
